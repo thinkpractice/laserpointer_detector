48b83b55ff480f876c4bc42defafed25fdb38d38
tjadejong <tjadejong@thinkpractice.nl>
2017-07-20 17:12:13 +0200

    Simple thresholding

--- LaserPointer.cpp
+++ LaserPointer.cpp
@@ -13,16 +13,21 @@ Ptr<BackgroundSubtractor> pMog2;
 void detectAndDisplay(Mat image, int i)
 {
     namedWindow("Image",WINDOW_AUTOSIZE);
+    Mat filteredImage;
+    medianBlur(image, filteredImage, 9);
+
     Mat bgr[3];
-    split(image, bgr);
+    split(filteredImage, bgr);
 
     Mat foregroundImage;
     pMog2->apply(bgr[2],foregroundImage);
     
-    Mat filteredImage;
-    medianBlur(foregroundImage, filteredImage, 9);
-    
-    imshow("Image",filteredImage);
+    Mat threshold1;
+    Mat threshold2;
+    Mat threshold3;
+    threshold(bgr[0], threshold1, 250, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+
+    imshow("Image",threshold1 & threshold2 & threshold3);
 }
 
 int main(int argc, char** argv)
