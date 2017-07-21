46041d1cef78f7ed227019b3f435b3a550043668
tjadejong <tjadejong@thinkpractice.nl>
2017-07-21 18:21:22 +0200

    Started implementation with blob detector

--- LaserPointer.cpp
+++ LaserPointer.cpp
@@ -51,25 +51,30 @@ void detectAndDisplay(Mat image, int i)
     Mat filteredImage;
     medianBlur(image, filteredImage, 9);
 
-    Mat hsv_image;
-    cvtColor(filteredImage, hsv_image, cv::COLOR_BGR2HSV);
+    Mat hsvImage;
+    cvtColor(filteredImage, hsvImage, cv::COLOR_BGR2HSV);
 
     Mat lowerRedHueRange;
     Mat upperRedHueRange;
     
-    inRange(hsv_image, Scalar(0,100,100), Scalar(10, 255, 255), lowerRedHueRange);
-    inRange(hsv_image, Scalar(160,100,100), Scalar(179, 255, 255),upperRedHueRange);
+    inRange(hsvImage, Scalar(0,100,100), Scalar(10, 255, 255), lowerRedHueRange);
+    inRange(hsvImage, Scalar(160,100,100), Scalar(179, 255, 255),upperRedHueRange);
 
     Mat redHueImage;
     addWeighted(lowerRedHueRange,1.0, upperRedHueRange,1.0, 0.0, redHueImage);
 
-    vector<Vec3f> circles;
-    //HoughCircles(redHueImage, circles, HOUGH_GRADIENT,2, redHueImage.rows / 16, 100, 20, 0, 0);
-    ////printf("Number of detected circles: %d\n",(int)circles.size());
+    vector<KeyPoint> keypoints;
+    SimpleBlobDetector detector;
+    detector.detect(redHueImage, keypoints);
+
+    //drawKeypoints(image, keypoints, image, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    //vector<Vec3f> circles;
+    //HoughCircles(redHueImage, circles, HOUGH_GRADIENT,2, 10, 100, 50, 0, 0);
+    //////printf("Number of detected circles: %d\n",(int)circles.size());
     //for(Vec3i c : circles)
     //{
     //    circle(image, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, LINE_AA);
-    //    circle(image, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, LINE_AA);
+    //    circle(image, Point(c[0], c[1]), 2, Scalar(0,0,255), 3, LINE_AA);
     //}
     imshow("Original Image",image);
     imshow("Image",redHueImage);
