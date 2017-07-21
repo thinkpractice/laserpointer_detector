a5989c4b360ebc040b3a9f1160a508a8112cabde
tjadejong <tjadejong@thinkpractice.nl>
2017-07-21 16:18:06 +0200

    Added circle overlay to camera image to simulate laser pointer

--- LaserPointer.cpp
+++ LaserPointer.cpp
@@ -4,12 +4,46 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <vector>
+#include <cstdlib>
 using namespace cv;
 using namespace std;
 
 Mat background;
 Ptr<BackgroundSubtractor> pMog2;
-  
+
+vector<Vec6i> overlayCircles;
+
+void initOverlayCircles(Mat image, int numberOfCircles=150)
+{
+    for (int i = 0; i < numberOfCircles; i++)
+    {
+        int x = rand() % image.rows;
+        int y = rand() % image.cols;
+        int size = rand() % 10;
+        int dx = rand() % 10;
+        int dy = rand() % 10;
+        Vec6i overlayCircle(x,y,size,dx,dy,0);
+
+        overlayCircles.push_back(overlayCircle);
+    }
+}
+
+void drawOverlayCircles(Mat image, vector<Vec6i>& circles)
+{
+    for (Vec6i& overlayCircle : circles)
+    {
+        overlayCircle[0] += overlayCircle[3];
+        overlayCircle[1] += overlayCircle[4];
+        if (overlayCircle[0] <= 0 || overlayCircle[0] >= image.cols)
+            overlayCircle[3] *= -1;
+
+        if (overlayCircle[1] <= 0 || overlayCircle[1] >= image.cols)
+            overlayCircle[4] *= -1;
+
+        circle(image, Point(overlayCircle[0], overlayCircle[1]), overlayCircle[2], Scalar(0,0,255), 3, LINE_AA);
+    }
+}
+
 void detectAndDisplay(Mat image, int i)
 {
     namedWindow("Original Image",WINDOW_AUTOSIZE);
@@ -28,16 +62,15 @@ void detectAndDisplay(Mat image, int i)
 
     Mat redHueImage;
     addWeighted(lowerRedHueRange,1.0, upperRedHueRange,1.0, 0.0, redHueImage);
-    medianBlur(redHueImage, redHueImage, 3);
 
     vector<Vec3f> circles;
-    HoughCircles(redHueImage, circles, HOUGH_GRADIENT,2, redHueImage.rows / 16, 100, 20, 0, 0);
-    //printf("Number of detected circles: %d\n",(int)circles.size());
-    for(Vec3i c : circles)
-    {
-        circle(image, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, LINE_AA);
-        circle(image, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, LINE_AA);
-    }
+    //HoughCircles(redHueImage, circles, HOUGH_GRADIENT,2, redHueImage.rows / 16, 100, 20, 0, 0);
+    ////printf("Number of detected circles: %d\n",(int)circles.size());
+    //for(Vec3i c : circles)
+    //{
+    //    circle(image, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, LINE_AA);
+    //    circle(image, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, LINE_AA);
+    //}
     imshow("Original Image",image);
     imshow("Image",redHueImage);
 }
@@ -60,7 +93,12 @@ int main(int argc, char** argv)
     {
         if (frame.empty())
             break;
+        if (i == 0)
+            initOverlayCircles(frame);
+
+        drawOverlayCircles(frame, overlayCircles);
         detectAndDisplay(frame,i);
+        
         char c = (char)waitKey(10);
         if (c == 27)
             break;
